f002307e5f29ebf4235c6664a9a1f104ceaaf511
Pronckunas <pedro_pronckunas@outlook.com>
2023-12-14 16:51:52 -0300

    Queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -201,6 +202,7 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 
 
 
@@ -219,7 +221,7 @@ public class ChessMatch {
         placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
-
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 
 
 	/*	placeNewPiece('a', 1, new Rook(board, Color.WHITE));
